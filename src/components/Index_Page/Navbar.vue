e63fd9c502df8b7d3d8e37bf7f9d9d539a74cfe0
Christian <vilcapomaparejaca1998@gmail.com>
2023-09-30 14:25:33 -0500

    Tabla Productos

--- src/components/Index_Page/Navbar.vue
+++ src/components/Index_Page/Navbar.vue
@@ -1,52 +1,11 @@
-<script setup>
-import { LIST_BUCKETS_MINIO } from '../../Util/Call_Api'
-import { GET_ALL_PRODUCTS } from '../../Util/Call_Api'
-import { ref, onMounted } from 'vue'
-const bucketsData = ref([]);
-const obtenerDatos = async () => {
-  try {
-    const data = await GET_ALL_PRODUCTS();
-    bucketsData.value = data;
-  } catch (error) {
-    console.error('Error al obtener datos:', error);
-  }
-};
-
-onMounted(() => {
-  obtenerDatos();
-});
-</script>
 <template>
   <section class="bg-blue-customize w-full flex justify-center">
     <div class="navbar bg-base-100 rounded-lg w-fit my-5">
-      <a class="btn btn-ghost normal-case text-xl">Menestras</a>
-      <a class="btn btn-ghost normal-case text-xl">Bebidas</a>
-      <a class="btn btn-ghost normal-case text-xl">Lacteos</a>
-      <a class="btn btn-ghost normal-case text-xl">Aseo personal</a>
-      <a class="btn btn-ghost normal-case text-xl">Limpieza</a>
-      <a class="btn btn-ghost normal-case text-xl">Comestibles</a>
-      <a class="btn btn-ghost normal-case text-xl">Masacotas</a>
-      <a class="btn btn-ghost normal-case text-xl">Herramientas</a>
-      <a class="btn btn-ghost normal-case text-xl">Dispositivos electronicos</a>
+      <a class="btn btn-ghost normal-case text-xl">PRODUCTOS</a>
+      <a class="btn btn-ghost normal-case text-xl">INGRESOS</a>
+      <a class="btn btn-ghost normal-case text-xl">RETIROS</a>
+      <a class="btn btn-ghost normal-case text-xl">EXISTENCIAS</a>
+      <a class="btn btn-ghost normal-case text-xl">REPORTES</a>
     </div>
   </section>
-  <table class="w-full bg-blue-500 text-white">
-  <thead>
-    <tr>
-      <th class="px-4 py-2">ID</th>
-      <th class="px-4 py-2">Nombre</th>
-      <th class="px-4 py-2">Comentario</th>
-      <th class="px-4 py-2">Cantidad</th>
-      <th class="px-4 py-2">Precio</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="item in bucketsData" :key="item.id" class="bg-gray-100 hover:bg-blue-200">
-      <td class="px-4 py-2">{{ item.id }}</td>
-      <td class="px-4 py-2">{{ item.comentario }}</td>
-      <td class="px-4 py-2">{{ item.cantidad }}</td>
-      <td class="px-4 py-2">{{ item.precio }}</td>
-    </tr>
-  </tbody>
-</table>
 </template>
---
+++ src/components/Index_Page/Products.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { LIST_BUCKETS_MINIO } from '../../Util/Call_Api'
+import { GET_ALL_PRODUCTS } from '../../Util/Call_Api'
+import { ref, onMounted } from 'vue'
+const bucketsData = ref([]);
+const obtenerDatos = async () => {
+  try {
+    const data = await GET_ALL_PRODUCTS();
+    bucketsData.value = data;
+  } catch (error) {
+    console.error('Error al obtener datos:', error);
+  }
+};
+
+onMounted(() => {
+  obtenerDatos();
+});
+</script>
+<template>
+<table class="w-full bg-blue-500 text-white">
+    <thead>
+      <tr>
+        <th class="px-4 py-2">ID</th>
+        <th class="px-4 py-2">Nombre</th>   
+        <th class="px-4 py-2">Comentario</th>
+        <th class="px-4 py-2">Cantidad</th>
+        <th class="px-4 py-2">Precio</th>
+      </tr>
+
+    </thead>
+    <tbody>
+      <tr v-for="item in bucketsData" :key="item.id" class="bg-gray-100 hover:bg-blue-200">
+        <td class="px-4 py-2 text-black text-center">{{ item.id }}</td>
+        <td class="px-4 py-2 text-black text-center">{{ item.nombre }}</td>
+        <td class="px-4 py-2 text-black text-center">{{ item.comentario }}</td>
+        <td class="px-4 py-2 text-black text-center">{{ item.cantidad }}</td>
+        <td class="px-4 py-2 text-black text-center">{{ item.precio }}</td> 
+      </tr>
+    </tbody>
+  </table>
+  </template>
